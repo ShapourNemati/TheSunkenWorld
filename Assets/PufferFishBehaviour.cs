090b493cecdff7d71835b4c0f7636609395222c8
Shapour Nemati <shapournemati@gmail.com>
2016-11-18 23:44:03 +0100

    Now the pufferfish goes up and down

--- Assets/PufferFishBehaviour.cs
+++ Assets/PufferFishBehaviour.cs
@@ -6,6 +6,10 @@ public class PufferFishBehaviour : MonoBehaviour, ICapturable {
 
 	public const int DMG = 25;
 	public bool active = true;
+	public float UPPER_LIMIT = 1f;
+	public float LOWER_LIMIT = -1f;
+	public float speed = 0;
+	public bool up = true;
 
 	public void GetCaptured()
 	{
@@ -22,7 +26,16 @@ public class PufferFishBehaviour : MonoBehaviour, ICapturable {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.position = transform.position + new Vector3 (0, speed * Time.deltaTime, 0);
+		if (up) {
+			speed += Time.deltaTime;
+			if (speed >= UPPER_LIMIT)
+				up = false;
+		} else {
+			speed -= Time.deltaTime;
+			if (speed <= LOWER_LIMIT)
+				up = true;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
