02fe8c0fb58d45d7d971751fdd6e3baf27b466cc
Shapour Nemati <shapournemati@gmail.com>
2016-11-18 22:39:25 +0100

    Added collision detection and a mermaid

---
+++ Assets/MermaidBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class MermaidBehaviour : MonoBehaviour, ICapturable {
+
+	public bool active = true;
+
+	public void GetCaptured()
+	{
+		active = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (active)
+			Debug.Log ("I'm harming the player. I swear");
+		else
+			Debug.Log ("Don't you worry don't you worry child");
+	}
+
+}
---
+++ Assets/Scripts/ICapturable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public interface ICapturable
+	{
+
+		void GetCaptured();
+
+	}
+}
+
