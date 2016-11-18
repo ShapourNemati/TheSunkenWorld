c17e801ccb45dc2f314a2f2405aa01dc1511ff88
Shapour Nemati <shapournemati@gmail.com>
2016-11-19 00:56:24 +0100

    Created the trident

---
+++ Assets/TridentBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TridentBehaviour : MonoBehaviour {
+
+	Vector3 startingPosition;
+	public float distance = 10f;
+	public float speed = 10f;
+	public float delay = 1f;
+	bool moving = false;
+
+	// Use this for initialization
+	void Start () {
+		startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (!moving) {
+			delay -= Time.deltaTime;
+			if (delay <= 0)
+				moving = true;
+		} else {
+			transform.position = transform.position + new Vector3 (0, speed * Time.deltaTime, 0);
+			if (transform.position.y - startingPosition.y >= distance) {
+				GameObject.Destroy (gameObject);
+			}
+		}
+
+	}
+}
