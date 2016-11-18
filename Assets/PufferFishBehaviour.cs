9faaaf90650bf5063e882eec762953b1d0940528
Shapour Nemati <shapournemati@gmail.com>
2016-11-18 23:23:57 +0100

    Implemented pufferfish

---
+++ Assets/PufferFishBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class PufferFishBehaviour : MonoBehaviour, ICapturable {
+
+	public const int DMG = 25;
+	public bool active = true;
+
+	public void GetCaptured()
+	{
+		//active = false;
+		// do nothing, PufferFish pops the bubbles
+		Debug.Log ("The pufferfish pops the bubble!");
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (active) {
+			var playerScript = other.GetComponent<PlayerScript>();
+			if (playerScript != null) {
+				playerScript.TakeDamage (DMG);
+			}
+		}
+	}
+}
