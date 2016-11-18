cdbacb8e56ebf1ff58137cc0c295bdf88de1205a
Shapour Nemati <shapournemati@gmail.com>
2016-11-18 21:22:00 +0100

    Created a really basic player with movement

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	float speed = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		HandleInput ();
+
+	}
+
+	void HandleInput()
+	{
+		if (Input.GetKey (KeyCode.A))
+			transform.position = transform.position - new Vector3 (speed * Time.deltaTime, 0, 0);
+		if (Input.GetKey (KeyCode.S))
+			transform.position = transform.position - new Vector3 (0, speed * Time.deltaTime, 0);
+		if (Input.GetKey (KeyCode.D))
+			transform.position = transform.position + new Vector3 (speed * Time.deltaTime, 0, 0);
+		if (Input.GetKey (KeyCode.W))
+			transform.position = transform.position + new Vector3 (0, speed * Time.deltaTime, 0);
+	}
+}
