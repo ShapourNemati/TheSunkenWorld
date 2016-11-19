b2cc59c6be092cff43f77930088672cb5f7b39bb
Shapour Nemati <shapournemati@gmail.com>
2016-11-19 11:49:52 +0100

    Created script that activates spawners when the camera sees them. Also added rigidbody and collider to TriggerSpawner prefab

---
+++ Assets/ActivateSpawners.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateSpawners : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		TriggerSpawner SpawnerScript = other.GetComponent<TriggerSpawner>();
+		if (SpawnerScript != null) {
+			SpawnerScript.Spawn ();
+		}
+	}
+}
+
