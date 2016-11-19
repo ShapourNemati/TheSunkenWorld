ca6e644c72adbfe4662d996fa5ede3958edc34a2
Shapour Nemati <shapournemati@gmail.com>
2016-11-19 05:28:47 +0100

    Added oxygen slider for UI

--- Assets/Scripts/Player/OxygenManager.cs
+++ Assets/Scripts/Player/OxygenManager.cs
@@ -1,23 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class OxygenManager : MonoBehaviour {
 
 	public const float STARTING_OXYGEN = 100f;
 	public const float OXYGEN_LOSS_PER_TIME = 2f;
 
-	
+	private Slider slider;
+
 	public float oxygen = STARTING_OXYGEN;
 
 	// Use this for initialization
 	void Start () {
-	
+		slider = GameObject.Find ("OxygenSlider").GetComponent <Slider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 		oxygen -= OXYGEN_LOSS_PER_TIME * Time.deltaTime;
+
+		//Update UI
+		slider.value = oxygen;
+
 		if (oxygen <= 0) {
 			//Something bad
 		}
