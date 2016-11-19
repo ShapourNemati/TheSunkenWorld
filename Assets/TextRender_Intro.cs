5e3df96e58451f75b5809d598c5c7e76176086fd
jona8690 <jona8690@edu.eal.dk>
2016-11-19 01:42:26 +0100

    Temp Intro text

---
+++ Assets/TextRender_Intro.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextRender_Intro : MonoBehaviour {
+
+	public Text MessageText;
+	public Dictionary<int, string> Text;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
