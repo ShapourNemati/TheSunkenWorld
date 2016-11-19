affdee050a020e8baddf74a7252be6165d831100
Shapour Nemati <shapournemati@gmail.com>
2016-11-19 04:46:25 +0100

    Created timed spawner

---
+++ Assets/TimedSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedSpawner : MonoBehaviour {
+
+	public GameObject creature;
+	public const float TIME_FOR_SPAWN = 5f;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = TIME_FOR_SPAWN;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if (timer <= 0) {
+			GameObject.Instantiate (creature,transform.position,Quaternion.identity);
+			timer = TIME_FOR_SPAWN;
+		}
+	}
+}
