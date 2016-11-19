ef4de6d0f77dcfc2ac89acbacc014d4521ef1b2c
jona8690 <jona8690@edu.eal.dk>
2016-11-19 03:43:38 +0100

    Intro Prolouge setup

---
+++ Assets/Scripts/SceneMasters/SceneSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+	public string NewScene;
+	public KeyCode Button;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(Button)) {
+			SceneManager.LoadScene(NewScene);
+		}
+	}
+}
