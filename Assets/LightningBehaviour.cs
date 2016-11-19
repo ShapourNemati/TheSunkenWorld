114c9d36f00a07016cf24585f1e95aa982ffe878
Shapour Nemati <shapournemati@gmail.com>
2016-11-19 03:36:54 +0100

    Created the actual lightning, which is working with Poseidon's skills

---
+++ Assets/LightningBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightningBehaviour : MonoBehaviour {
+
+	public float delay = 2f;
+
+	public int DMG = 30;
+
+	public BoxCollider2D myCollider;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		delay -= Time.deltaTime;
+		if (delay <= 0) {
+			myCollider.enabled = true;
+			if (delay <= -0.2f)
+				GameObject.Destroy (gameObject);
+		}
+	
+	}
+
+	public void OnTriggerStay2D(Collider2D other)
+	{
+		var playerScript = other.GetComponent<PlayerScript>();
+		if (playerScript != null) {
+			playerScript.TakeDamage (DMG);
+			GameObject.Destroy (gameObject);
+		}
+	}
+}
