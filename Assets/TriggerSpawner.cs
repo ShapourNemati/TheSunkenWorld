5a3207b2c71cf9578c3c550542d2209d5659614a
Shapour Nemati <shapournemati@gmail.com>
2016-11-19 04:52:40 +0100

    Created trigger spawner

---
+++ Assets/TriggerSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerSpawner : MonoBehaviour {
+
+	public GameObject creature;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Spawn()
+	{
+		GameObject.Instantiate (creature,transform.position,Quaternion.identity);
+	}
+}
