c69fb3a6670e18d697604546cd4a7d7a26a39aad
Shapour Nemati <shapournemati@gmail.com>
2016-11-19 03:50:16 +0100

    Imported some asstes and made Plato head rotate a bit

---
+++ Assets/SlightlyTilting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlightlyTilting : MonoBehaviour {
+
+	bool clockWise = true;
+	public const float DURATION = 1f;
+	public const float SPEED = 0.6f;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = 2*DURATION/3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			clockWise = !clockWise;
+			timer = DURATION;
+		}
+
+		transform.Rotate (new Vector3 (0, 0, SPEED * (clockWise ? 1 : -1)));
+	}
+}
